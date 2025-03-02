22c17aa0630c4f485bc82d9439295a9bedd0e782
gaffey <Songyjgaf@163.com>
2025-03-02 18:28:03 +0800

    开发注册、登录界面

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import {testUsingGet} from "../api/mainController";
+
+testUsingGet().then((res) => {
+  console.log(res)
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+
+  </main>
+</template>
